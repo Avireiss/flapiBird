b2df440818ce56ddfc396f0ecfe5a9cb29cc84c1
aviReiss <avi328176284@gmail.com>
2024-06-20 11:22:04 +0300

    add all

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Flappy Bird");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
+
+        MainMenu mainMenu = new MainMenu(frame);
+        frame.add(mainMenu);
+
+        frame.setVisible(true);
+        System.out.println("k");
+    }
+}
+
